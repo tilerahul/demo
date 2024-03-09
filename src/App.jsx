3aebdf228a4bea0dc281e7fb490d612b9a714950
Rahul Tile <tilerahul8068@gmail.com>
2024-03-09 08:30:35 +0530

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,8 @@ function App() {
     <>
       <div>
         <h1>This is demo project</h1>
+        <h2>The author of this site : Rahul Tile</h2>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
