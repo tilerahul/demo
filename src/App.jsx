4c14c455243e67b8eb257ac05ce2e08ccc062f2b
Rahul Tile <tilerahul8068@gmail.com>
2024-03-09 12:02:37 +0530

    new file added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
         <h1>This is demo project</h1>
-        <h2>The author of this site : Rahul Tile</h2>
         <h2>Successfully deploy the site on netlify</h2>
       </div>
     </>
